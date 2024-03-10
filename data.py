8d8eef11d4a108f7a06ac9699f25b9e8e3768d06
Andrew Glazko <glazko.ae@gmail.com>
2024-03-10 17:39:20 +0300

    init commit

---
+++ data.py
@@ -0,0 +1,8 @@
+def load_data() -> str:
+    """Загрузка датасета Iris."""
+    pass
+
+
+def prepare_data(csv_path: str) -> list[str]:
+    """Чтение загруженного датасета и разделение на train и test выборки."""
+    pass
